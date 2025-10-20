d2f3c15e8bfce2f81b5bdf07b7299c4726f0fd87
Alvaro LJ <114070010+Alvaro-LJ@users.noreply.github.com>
2025-10-20 13:43:49 +0200

    Improved shell command

--- R/Describe_my_computing_unit.R
+++ R/Describe_my_computing_unit.R
@@ -14,20 +14,18 @@ Describe_my_computing_unit <-
     #Proceed
     if(stringr::str_detect(benchmarkme::get_platform_info()$OS.type, "windows")){
       list(
-        CPU =  paste0("CPU: ", benchmarkme::get_cpu()$model_name, ". Number of available threads (cores): ", benchmarkme::get_cpu()$no_of_cores),
-        RAM =  paste0("Total available RAM memory: approximately ",
-                      round(
-                        as.double(substr(shell('wmic path Win32_ComputerSystem get TotalPhysicalMemory/value', translate = F, intern = TRUE)[[3]],
-                                         start = 21,
-                                         stop = nchar(shell('wmic path Win32_ComputerSystem get TotalPhysicalMemory/value', translate = F, intern = TRUE)[[3]])-2))/104857600,
-                        digits = 2),
-                      " Gb"),
-        R_VERSION =  paste0(benchmarkme::get_r_version()$version.string, " - ",
-                            benchmarkme::get_r_version()$nickname, ". Version type: ", benchmarkme::get_r_version()$arch),
-        OS = paste0("You are running R on ", benchmarkme::get_platform_info()$OS.type)
+        CPU =  try(paste0("CPU: ", benchmarkme::get_cpu()$model_name, ". Number of available threads (cores): ", benchmarkme::get_cpu()$no_of_cores)),
+        RAM =  try(paste0("Total available RAM memory: approximately ",
+                      shell('powershell -Command "& {\\"{0:N2} GB\\" -f ((Get-CimInstance -ClassName Win32_ComputerSystem).TotalPhysicalMemory / 1GB)}"', intern = TRUE))),
+        R_VERSION =  try(paste0(benchmarkme::get_r_version()$version.string, " - ",
+                            benchmarkme::get_r_version()$nickname, ". Version type: ", benchmarkme::get_r_version()$arch)),
+        OS = try(paste0("You are running R on ", benchmarkme::get_platform_info()$OS.type))
       )
     }
 
     else (
       benchmarkme::get_sys_details(byte_compiler = F, linear_algebra = F,  locale = F, installed_packages = F, machine = F)
     )}
+
+
+
