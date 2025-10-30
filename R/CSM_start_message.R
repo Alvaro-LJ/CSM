3c332ae30df76d0899ec5a92eefadcdad78cdd7c
Alvaro LJ <114070010+Alvaro-LJ@users.noreply.github.com>
2025-10-30 09:17:55 +0100

    Package version updated and start-up message

---
+++ R/CSM_start_message.R
@@ -0,0 +1,26 @@
+#' This regulates the package start-up message
+#'
+#'
+#' @param libname Used to obtain library name
+#' @param pkgname Used to obtain package name
+#'
+#' @keywords Internal
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage(
+    cat(
+      stringr::str_c("CSM (version ", utils::packageDescription("CSM")$Version, ")", " loaded"),
+      "",
+      "Please see package help with help(package = 'CSM')",
+      "",
+      "For further information please visit our github repositories: https://github.com/Alvaro-LJ",
+      "",
+      "Bug and suggestions can be reported through our github profile",
+      "",
+      "Thanks for using CSM!",
+      fill = 4
+    )
+  )
+}
+
+
+
